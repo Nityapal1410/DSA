bb0bd7a6dfb184a23bdf6ab0eb629a4081a39c8d
Nitya Pal <nityapal1410@gmail.com>
2026-06-26 23:17:22 +0530

    Time: 166 ms (67.77%) | Memory: 20.5 MB (79.59%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,23 @@
+class Solution:
+    def gethours(self,piles,mid):
+        ans = 0 
+        for piles in piles:
+            ans += (piles + mid - 1)// mid
+        return ans
+    
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+        l = 1
+        r = max(piles)
+
+        while l<=r:
+            mid= (l+r)//2
+            if self.gethours(piles,mid) >h:
+                l = mid + 1
+            else:
+                k= mid
+                r = mid - 1
+        return k
+
+        
