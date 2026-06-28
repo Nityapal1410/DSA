5cf01394450d1bfae0c4218af3578d382c860158
Nitya Pal <nityapal1410@gmail.com>
2026-06-28 22:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.14%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -4,17 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = head # WITHOUT SLOW AND FAST POINTERS
-        l = 0
-        while curr !=None:
-            curr = curr.next
-            l+=1
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:#WIRH SLOW AND FAST POINTER 
 
-        curr = head
-        for i in range(l//2):
-            curr = curr.next
-        
-          
-        return curr
+        slow = head
+        fast = head
+
+        while fast !=None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
         
