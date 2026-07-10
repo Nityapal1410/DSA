426f35717f36e127033afb5aa0d0ebb1d87189f5
Nitya Pal <nityapal1410@gmail.com>
2026-07-11 01:23:27 +0530

    Time: 3 ms (19.25%) | Memory: 20.1 MB (24.63%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -4,33 +4,58 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Node:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+class Queue:
+    def __init__(self):
+        self.q = []
+        self.front = 0   # index of front element
 
+    def push(self, x):
+        # enqueue element at the end
+        self.q.append(x)
+
+    def pop(self):
+        # dequeue element from the front
+        if self.size() == 0:
+            return None
+        x = self.q[self.front]
+        self.front += 1
+        return x
+
+    def getfront(self):
+        # return front element without removing
+        if self.size() == 0:
+            return None
+        return self.q[self.front]
+
+    def size(self):
+        # current size of queue
+        return len(self.q) - self.front
+
+    def empty(self):
+        return self.size() == 0
+
+        
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        
-        result = []
-        queue = [root]   # normal list used as queue
-        
-        while queue:
-            level = []
-            size = len(queue)
-            
-            for _ in range(size):
-                node = queue.pop(0)   # dequeue from front
-                level.append(node.val)
-                
-                if node.left:
-                    queue.append(node.left)   # enqueue left child
-                if node.right:
-                    queue.append(node.right)  # enqueue right child
-            
-            result.append(level)
+        ans = []
+        if root is None:
+            return ans
 
-        return result
+        queue =Queue()
+        queue.push(root)
+        ans.append([root.val])
+        
+        while queue.size()>0:
+            l = queue.size()
+            level =[]
+            for i in range(l):
+                front = queue.pop()
+                if front.left!=None:
+                    queue.push(front.left)
+                    level.append(front.left.val)
+                if front.right!=None:
+                    queue.push(front.right)
+                    level.append(front.right.val)
+            if len(level)>0:
+                ans.append(level)
+        return ans
